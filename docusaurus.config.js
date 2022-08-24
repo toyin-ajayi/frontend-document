b0e03b07278ba6ecb9ad47114bde904be45a10da
jianjiachenghub <1797731542@qq.com>
2022-08-24 20:29:59 +0800

    feat: 基础配置信息

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const sidebarTransform = require("./src/utils/sidebarTransform");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "JcJian's fontend document",
+  tagline: "构建前端知识体系",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -17,15 +17,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "jianjiacheng", // Usually your GitHub org/user name.
+  projectName: "fontend-document", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
   },
 
   presets: [
@@ -90,15 +90,6 @@ const config = {
       footer: {
         style: "dark",
         links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
           {
             title: "Community",
             items: [
